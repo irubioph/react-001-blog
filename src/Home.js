ccd607c5d3de2269f8629022180f5190adb4e3bc
IvanR <delightsmediaph@gmail.com>
2023-07-31 21:23:37 +0800

    Lesson 5: Multiple Compontents

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Navbar from './Navbar';
-import Home from './Home';
 
 function App() {
+
   return (
     <div className="App">
+      <Navbar></Navbar>
       <div className="content">
-        <Home />
-      </div>
+        <h1>Home</h1>   
+      </div>   
     </div>
   );
 }
