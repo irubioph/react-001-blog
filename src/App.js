18c9ee31f9bcb603d6692ba46309fcc95cddb8be
IvanR <delightsmediaph@gmail.com>
2023-07-31 21:32:15 +0800

    Lesson 5: Multiple Components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Home';
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
         <Home />
       </div>
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Navbar from './Navbar';
 
 function App() {
 
   return (
     <div className="App">
-      <Navbar></Navbar>
       <div className="content">
         <h1>Home</h1>   
       </div>   
